1ba1685c12937d577e3dfa1fd14ca0bb11850704
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-09-17 14:08:44 -0300

    elimination of validations

--- src/main/java/com/ecommerce/demo/entities/Client.java
+++ src/main/java/com/ecommerce/demo/entities/Client.java
@@ -17,39 +17,25 @@ public class Client {
     private Long id;
 
     @Column(nullable = false)
-    @NotBlank(message = "First name is required")
-    @Size(max = 50, message = "First name cannot exceed 50 characters")
     private String firstname;
 
     @Column(nullable = false)
-    @NotBlank(message = "Last name is required")
-    @Size(max = 50, message = "Last name cannot exceed 50 characters")
     private String lastname;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "Username is required")
-    @Size(max = 30, message = "Username cannot exceed 30 characters")
-    @Pattern(regexp = "^\\w+$", message = "The user name can only contain alphanumeric characters and underscores.")
     private String username;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
     private String email;
 
     @Column(nullable = false)
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @Column(nullable = false)
-    @NotNull(message = "Age is required")
-    @Min(value = 19, message = "Age must be greater than 18")
     private Integer age;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    @NotNull(message = "Gender is required")
     private Gender gender;
 
     @Column(name = "created_at", columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT now()")
@@ -74,59 +60,59 @@ public class Client {
         this.id = id;
     }
 
-    public @NotBlank(message = "First name is required") @Size(max = 50, message = "First name cannot exceed 50 characters") String getFirstname() {
+    public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(@NotBlank(message = "First name is required") @Size(max = 50, message = "First name cannot exceed 50 characters") String firstname) {
+    public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
-    public @NotBlank(message = "Last name is required") @Size(max = 50, message = "Last name cannot exceed 50 characters") String getLastname() {
+    public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(@NotBlank(message = "Last name is required") @Size(max = 50, message = "Last name cannot exceed 50 characters") String lastname) {
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    public @NotBlank(message = "Username is required") @Size(max = 30, message = "Username cannot exceed 30 characters") @Pattern(regexp = "^\\w+$", message = "The user name can only contain alphanumeric characters and underscores.") String getUsername() {
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(@NotBlank(message = "Username is required") @Size(max = 30, message = "Username cannot exceed 30 characters") @Pattern(regexp = "^\\w+$", message = "The user name can only contain alphanumeric characters and underscores.") String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public @NotBlank(message = "Email is required") @Email(message = "Email should be valid") String getEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public @NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(@NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String password) {
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public @NotNull(message = "Age is required") @Min(value = 19, message = "Age must be greater than 18") Integer getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(@NotNull(message = "Age is required") @Min(value = 19, message = "Age must be greater than 18") Integer age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
-    public @NotNull(message = "Gender is required") Gender getGender() {
+    public Gender getGender() {
         return gender;
     }
 
-    public void setGender(@NotNull(message = "Gender is required") Gender gender) {
+    public void setGender(Gender gender) {
         this.gender = gender;
     }
 
