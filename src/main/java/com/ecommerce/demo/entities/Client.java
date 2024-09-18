4ca7c1570c20964fa23f3ebb86445e9c01aeb72e
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-09-17 23:06:14 -0300

    client builder

--- src/main/java/com/ecommerce/demo/entities/Client.java
+++ src/main/java/com/ecommerce/demo/entities/Client.java
@@ -1,9 +1,10 @@
 package com.ecommerce.demo.entities;
 
+import com.ecommerce.demo.dto.request.ClientRequest;
 import com.ecommerce.demo.enums.Gender;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 
+import java.io.Serializable;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,33 @@ import java.util.Set;
 
 @Entity
 @Table(name = "Clients")
-public class Client {
+public class Client implements Serializable {
+    public static class Builder {
+        private Long id;
+        private String firstname;
+        private String lastname;
+        private String username;
+        private String email;
+        private Integer age;
+        private Gender gender;
+        private ZonedDateTime createdAt;
+        private ZonedDateTime updatedAt;
+
+        public Builder id(Long id) { this.id = id; return this; }
+        public Builder firstname(String firstname) { this.firstname = firstname; return this; }
+        public Builder lastname(String lastname) { this.lastname = lastname; return this; }
+        public Builder username(String username) { this.username = username; return this;}
+        public Builder email(String email) { this.email = email; return this; }
+        public Builder age(Integer age) { this.age = age; return this; }
+        public Builder gender(Gender gender) { this.gender = gender; return this; }
+        public Builder createdAt(ZonedDateTime createdAt) {this.createdAt = createdAt; return this; }
+        public Builder updatedAt(ZonedDateTime updatedAt) {this.updatedAt = updatedAt; return this; }
+
+        public Client build() {
+            return new Client(this);
+        }
+
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -51,6 +78,29 @@ public class Client {
     @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("createdAt")
     private List<Cart> carts = new ArrayList<>();
+
+    public Client(Builder builder) {
+        this.id = builder.id;
+        this.firstname = builder.firstname;
+        this.lastname = builder.lastname;
+        this.username = builder.username;
+        this.email = builder.email;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.createdAt = builder.createdAt;
+        this.updatedAt = builder.updatedAt;
+    }
+
+    public static Client toClientResponse(ClientRequest request) {
+        return new Client.Builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .age(request.getAge())
+                .gender(request.getGender())
+                .build();
+    }
 
     public Long getId() {
         return id;
