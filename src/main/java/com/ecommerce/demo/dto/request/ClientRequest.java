f3940ecfa996de51a807b153fa17d2c620da590e
José Ignacio Herrera Gré <125605603+Dexiotelio@users.noreply.github.com>
2024-09-26 00:44:54 -0300

    Update ClientRequest.java

--- src/main/java/com/ecommerce/demo/dto/request/ClientRequest.java
+++ src/main/java/com/ecommerce/demo/dto/request/ClientRequest.java
@@ -1,118 +1,111 @@
 package com.ecommerce.demo.dto.request;
 
-import com.ecommerce.demo.entities.Client;
 import com.ecommerce.demo.enums.Gender;
+import com.ecommerce.demo.enums.Role;
 import jakarta.validation.constraints.*;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
-public class ClientRequest implements Serializable {
-
-    @NotBlank(message = "Id is requires")
-    private Long id;
+public class UserRequest implements Serializable {
 
     @NotBlank(message = "First name is required")
     @Size(max = 50, message = "First name cannot exceed 50 characters")
-    private String firstname;
+    private final String firstName;
 
     @NotBlank(message = "Last name is required")
     @Size(max = 50, message = "Last name cannot exceed 50 characters")
-    private String lastname;
+    private final String lastName;
 
     @NotBlank(message = "Username is required")
     @Size(max = 30, message = "Username cannot exceed 30 characters")
     @Pattern(regexp = "^\\w+$", message = "Username can only contain alphanumeric characters and underscores")
-    private String username;
+    private final String userName;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
-    private String email;
+    private final String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
-    private String password;
+    private final String password;
 
     @NotNull(message = "Age is required")
     @Min(value = 19, message = "Age must be greater than 18")
-    private Integer age;
+    private final Integer age;
 
     @NotNull(message = "Gender is required")
-    private Gender gender;
+    private final Gender gender;
 
-    public @NotBlank(message = "Id is requires") Long getId() {
-        return id;
+    @NotNull(message = "Phones are required")
+    private final String[] phones;
+
+    @NotNull(message = "Role is required")
+    private final Role role;
+
+    // Constructor
+    public UserRequest(String firstName, String lastName, String userName, String email,
+                       String password, Integer age, Gender gender, String[] phones, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+        this.phones = phones;
+        this.role = role;
     }
 
-    public void setId(@NotBlank(message = "Id is requires") Long id) {
-        this.id = id;
+    // Getters
+    public String getFirstName() {
+        return firstName;
     }
 
-    public @NotBlank(message = "First name is required") @Size(max = 50, message = "First name cannot exceed 50 characters") String getFirstname() {
-        return firstname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setFirstname(@NotBlank(message = "First name is required") @Size(max = 50, message = "First name cannot exceed 50 characters") String firstname) {
-        this.firstname = firstname;
+    public String getUserName() {
+        return userName;
     }
 
-    public @NotBlank(message = "Last name is required") @Size(max = 50, message = "Last name cannot exceed 50 characters") String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(@NotBlank(message = "Last name is required") @Size(max = 50, message = "Last name cannot exceed 50 characters") String lastname) {
-        this.lastname = lastname;
-    }
-
-    public @NotBlank(message = "Username is required") @Size(max = 30, message = "Username cannot exceed 30 characters") @Pattern(regexp = "^\\w+$", message = "Username can only contain alphanumeric characters and underscores") String getUsername() {
-        return username;
-    }
-
-    public void setUsername(@NotBlank(message = "Username is required") @Size(max = 30, message = "Username cannot exceed 30 characters") @Pattern(regexp = "^\\w+$", message = "Username can only contain alphanumeric characters and underscores") String username) {
-        this.username = username;
-    }
-
-    public @NotBlank(message = "Email is required") @Email(message = "Email should be valid") String getEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email) {
-        this.email = email;
-    }
-
-    public @NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(@NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String password) {
-        this.password = password;
-    }
-
-    public @NotNull(message = "Age is required") @Min(value = 19, message = "Age must be greater than 18") Integer getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(@NotNull(message = "Age is required") @Min(value = 19, message = "Age must be greater than 18") Integer age) {
-        this.age = age;
-    }
-
-    public @NotNull(message = "Gender is required") Gender getGender() {
+    public Gender getGender() {
         return gender;
     }
 
-    public void setGender(@NotNull(message = "Gender is required") Gender gender) {
-        this.gender = gender;
+    public String[] getPhones() {
+        return phones;
+    }
+
+    public Role getRole() {
+        return role;
     }
 
     @Override
     public String toString() {
-        return "ClientRequest{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", username='" + username + '\'' +
+        return "UserRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", age=" + age +
                 ", gender=" + gender +
+                ", phones=" + Arrays.toString(phones) +
+                ", role=" + role +
                 '}';
     }
 }
