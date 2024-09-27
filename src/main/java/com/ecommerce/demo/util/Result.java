bb787b9a777054fd8032b0672e8743cff301b672
José Ignacio Herrera Gré <125605603+Dexiotelio@users.noreply.github.com>
2024-09-26 23:46:45 -0300

    Update Result.java

--- src/main/java/com/ecommerce/demo/util/Result.java
+++ src/main/java/com/ecommerce/demo/util/Result.java
@@ -1,5 +1,21 @@
 package com.ecommerce.demo.util;
 
 public class Result<T> {
-    
+    private final T value;
+    private final boolean isSuccees;
+    private final String error;
+
+    private Result(T value, boolean isSuccees, String error ) {
+        this.value = value;
+        this.isSuccees = isSuccees;
+        this.error = error;
+    }
+
+    public static <T> Result<T> success(T value) {
+        return new Result<>(value, true, null);
+    }
+
+    public static <T> Result<T> failure(String error) {
+        return new Result<>(null, false, error);
+    }
 }
