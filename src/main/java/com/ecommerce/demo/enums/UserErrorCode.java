fa84cf5f62d5c030c6258058d106916f163cfda8
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-09-30 23:22:57 -0300

    update UserErrorCode

--- src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
+++ src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
@@ -2,7 +2,9 @@ package com.ecommerce.demo.enums;
 
 public enum UserErrorCode {
     USER_ALREADY_EXISTS("The user already exists"),
-    USER_CREATION_FAILURE("Failed to create user");
+    USER_CREATION_FAILURE("Failed to create user"),
+    USER_AGE_FAILURE("Age must be greater than 18"),
+    USER_USERNAME_EXISTS("The username already exists");
 
     private final String message;
 
