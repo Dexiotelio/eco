61f89a8113e6a63d42a5846c0cb778c9381be977
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-01 16:25:47 -0300

    update UserErrorCode

--- src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
+++ src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
@@ -1,10 +1,18 @@
 package com.ecommerce.demo.enums;
 
 public enum UserErrorCode {
-    USER_ALREADY_EXISTS("The user already exists"),
-    USER_CREATION_FAILURE("Failed to create user"),
-    USER_AGE_FAILURE("Age must be greater than 18"),
-    USER_USERNAME_EXISTS("The username already exists");
+    USER_ALREADY_EXISTS("The user already exists."),
+    USER_CREATION_FAILURE("Failed to create user."),
+    USER_AGE_FAILURE("Age must be greater than 18."),
+    USER_USERNAME_EXISTS("The username already exists."),
+    USER_FIRSTNAME_LENGTH_FAILURE("First name must be between 3 and 50 characters."),
+    USER_LASTNAME_LENGTH_FAILURE("Last name must be between 3 and 50 characters."),
+    USER_USERNAME_LENGTH_FAILURE("User name must be between 3 and 50 characters."),
+    USER_PASSWORD_LENGTH_FAILURE("Password must be at least 8 characters long."),
+    USER_GENDER_FAILURE("Gender must be 'male', 'female', or 'other'."),
+    USER_ROLE_FAILURE("Role must be 'client', 'admin', or 'visitor'."),
+    USER_PHONES_FAILURE("At least one phone number is required."),
+    USER_EMAIL_FORMAT_FAILURE("Email format is invalid.");
 
     private final String message;
 
