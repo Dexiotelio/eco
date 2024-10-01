4d3191e80b790532fca795d4d0da3bec26a27232
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-01 20:26:56 -0300

    implement validation

---
+++ src/main/java/com/ecommerce/demo/services/AddressWriteServicesImpl.java
@@ -0,0 +1,61 @@
+package com.ecommerce.demo.services;
+
+import com.ecommerce.demo.dto.request.AddressRequest;
+import com.ecommerce.demo.dto.response.AddressResponse;
+import com.ecommerce.demo.entities.Address;
+import com.ecommerce.demo.enums.AddressErrorCode;
+import com.ecommerce.demo.repositories.AddressQueryRepositoryImpl;
+import com.ecommerce.demo.repositories.AddressWriteRepositoryImpl;
+import com.ecommerce.demo.util.AddressValidation;
+import com.ecommerce.demo.util.Result;
+import io.vavr.control.Either;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Service
+public class AddressWriteServicesImpl implements AddressWriteServices {
+    private final AddressWriteRepositoryImpl addressWriteRepository;
+    private final AddressQueryRepositoryImpl addressQueryRepository;
+
+    @Autowired
+    public AddressWriteServicesImpl(AddressWriteRepositoryImpl addressWriteRepository,
+                                    AddressQueryRepositoryImpl addressQueryRepository) {
+        this.addressWriteRepository = addressWriteRepository;
+        this.addressQueryRepository = addressQueryRepository;
+    }
+
+    @Override
+    @Transactional
+    public Result<AddressResponse> create(AddressRequest request) {
+        Either<Set<String>, AddressRequest> addressValidation = AddressValidation.validateAddressRequest(request);
+        if (addressValidation.isLeft()) {
+            return Result.failure(addressValidation.getLeft());
+        }
+        if (!addressQueryRepository.exists(request).isSuccess()) {
+            return Result.failure(AddressErrorCode.ADDRESS_ALREADY_EXISTS.getMessage());
+        }
+
+        Address address = Address.toAddress(request);
+        Result<Void> addressCreationResult = addressWriteRepository.create(address);
+        if (addressCreationResult.isFailure()) {
+            return Result.failure(AddressErrorCode.ADDRESS_CREATION_FAILURE
+                    + String.join(", ", addressCreationResult.getErrors()));
+        }
+
+        AddressResponse response = AddressResponse.toAddressResponse(address);
+        return Result.success(response);
+    }
+
+    @Override
+    public Result<AddressResponse> update(AddressRequest request) {
+        return null;
+    }
+
+    @Override
+    public Result<AddressResponse> delete(AddressRequest request) {
+        return null;
+    }
+}
