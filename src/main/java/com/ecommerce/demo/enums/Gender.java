0f53c48ae17d60d84f580cd8f71c23bac869b547
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-02 20:51:45 -0300

    update Gender

--- src/main/java/com/ecommerce/demo/enums/Gender.java
+++ src/main/java/com/ecommerce/demo/enums/Gender.java
@@ -1,7 +1,17 @@
 package com.ecommerce.demo.enums;
 
 public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
+    MALE("male"),
+    FEMALE("female"),
+    OTHER("other");
+
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
