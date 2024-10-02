337b7cc652c397360a3c1a138215adf74256024b
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-02 20:52:29 -0300

    update UserErrorCode

--- src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
+++ src/main/java/com/ecommerce/demo/enums/UserErrorCode.java
@@ -8,11 +8,13 @@ public enum UserErrorCode {
     USER_FIRSTNAME_LENGTH_FAILURE("First name must be between 3 and 50 characters."),
     USER_LASTNAME_LENGTH_FAILURE("Last name must be between 3 and 50 characters."),
     USER_USERNAME_LENGTH_FAILURE("User name must be between 3 and 50 characters."),
-    USER_PASSWORD_LENGTH_FAILURE("Password must be at least 8 characters long."),
+    USER_PASSWORD_LENGTH_FAILURE("Password must be at least 10 characters long."),
     USER_GENDER_FAILURE("Gender must be 'male', 'female', or 'other'."),
     USER_ROLE_FAILURE("Role must be 'client', 'admin', or 'visitor'."),
-    USER_PHONES_FAILURE("At least one phone number is required."),
-    USER_EMAIL_FORMAT_FAILURE("Email format is invalid.");
+    USER_PHONES_FAILURE("At least one phone number is required. You must provide between 1 and 2 phone numbers."),
+    USER_EMAIL_FORMAT_FAILURE("Email format is invalid."),
+    USER_PHONE_FORMAT_FAILURE("Phone number format is invalid. It must start with '+' " +
+            "followed by 1 to 3 digits for the country code, and then 4 to 14 digits for the phone number.");
 
     private final String message;
 
