03439a2d9c5f9791bf077c4487f47fe46acfa3fd
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-03 16:57:05 -0300

    update UserWriteServicesImpl

--- src/main/java/com/ecommerce/demo/services/UserWriteServicesImpl.java
+++ src/main/java/com/ecommerce/demo/services/UserWriteServicesImpl.java
@@ -4,28 +4,27 @@ import com.ecommerce.demo.dto.request.AddressRequest;
 import com.ecommerce.demo.dto.request.UserRequest;
 import com.ecommerce.demo.dto.response.AddressResponse;
 import com.ecommerce.demo.dto.response.UserResponse;
-import com.ecommerce.demo.entities.Address;
 import com.ecommerce.demo.entities.User;
 import com.ecommerce.demo.enums.UserErrorCode;
-import com.ecommerce.demo.repositories.AddressQueryRepositoryImpl;
-import com.ecommerce.demo.repositories.AddressWriteRepositoryImpl;
 import com.ecommerce.demo.repositories.UserQueryRepositoryImpl;
 import com.ecommerce.demo.repositories.UserWriteRepositoryImpl;
+import com.ecommerce.demo.services.interfaces.UserWriteServices;
 import com.ecommerce.demo.util.Result;
-import com.ecommerce.demo.util.UserValidation;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.function.Predicate;
 
 // Service implementation for user writing operations
 @Service
 public class UserWriteServicesImpl implements UserWriteServices {
+    private static final Logger logger = LoggerFactory.getLogger(UserWriteServicesImpl.class);
+
     private final UserWriteRepositoryImpl userWriteRepository;
     private final UserQueryRepositoryImpl userQueryRepository;
     private final AddressWriteServicesImpl addressWriteServices;
@@ -46,28 +45,33 @@ public class UserWriteServicesImpl implements UserWriteServices {
         // Validate the user request
         Either<Set<String>, UserRequest> userValidation = UserValidation.validateUserRequest(request);
         if (userValidation.isLeft()) {
+            logger.warn("Errores de validación para el usuario: {}", userValidation.getLeft());
             return Result.failure(userValidation.getLeft()); // Return validation errors if present
         }
 
         // Check if the user already exists based on the email
         Try<Result<Boolean>> userExists = userQueryRepository.exists(request.getEmail());
         if (userExists.isFailure()) {
+            logger.error("Error al verificar la existencia del usuario: {}", userExists.getCause().getMessage());
             return Result.failure(userExists.getCause().getMessage()); // Handle failure in querying
         }
 
         Result<Boolean> userExistsResult = userExists.get();
         if (userExistsResult.isSuccess() && Boolean.TRUE.equals(userExistsResult.getValue())) {
+            logger.warn("El usuario ya existe: {}", request.getEmail());
             return Result.failure(UserErrorCode.USER_ALREADY_EXISTS.getMessage()); // Return error if user exists
         }
 
         // Check if the username already exists
         Try<Result<Boolean>> usernameExists = userQueryRepository.existsUsername(request.getUserName());
         if (usernameExists.isFailure()) {
+            logger.error("Error al verificar la existencia del nombre de usuario: {}", usernameExists.getCause().getMessage());
             return Result.failure(usernameExists.getCause().getMessage()); // Handle failure in querying
         }
 
         Result<Boolean> usernameExistsResult = usernameExists.get();
         if (usernameExistsResult.isSuccess() && Boolean.TRUE.equals(usernameExistsResult.getValue())) {
+            logger.warn("El nombre de usuario ya existe: {}", request.getUserName());
             return Result.failure(UserErrorCode.USER_USERNAME_EXISTS.getMessage()); // Return error if username exists
         }
 
@@ -75,23 +79,27 @@ public class UserWriteServicesImpl implements UserWriteServices {
         User user = User.toUser(request);
         Result<Long> userCreationResult = userWriteRepository.create(user);
         if (userCreationResult.isFailure()) {
+            logger.error("Error al crear el usuario: {}", userCreationResult.getErrors());
             return Result.failure(UserErrorCode.USER_CREATION_FAILURE.getMessage() + ": "
                     + String.join(", ", userCreationResult.getErrors())); // Return error if user creation fails
         }
 
         Long userId = userCreationResult.getValue();
+        logger.info("Usuario creado con éxito: ID {}", userId);
         // Create addresses associated with the user
         for (AddressRequest addressRequest : request.getAddress()) {
             Result<AddressResponse> addressResponseResult = addressWriteServices.create(userId, addressRequest);
             if (addressResponseResult.isFailure()) {
                 // Uncomment to delete user if address creation fails
                 String errorsMessages = String.join(", ", addressResponseResult.getErrors());
+                logger.error("Error al crear la dirección para el usuario ID {}: {}", userId, errorsMessages);
                 return Result.failure(String.format(
                         UserErrorCode.USER_ADDRESS_CREATION_FAILURE.getMessage(), errorsMessages));
             }
         }
         // Convert the user entity to response format and return success
         UserResponse userResponse = UserResponse.toUserResponse(user);
+        logger.info("Proceso de creación de usuario finalizado con éxito: {}", userResponse);
         return Result.success(userResponse);
     }
 
