30bcc4a1a32469b269d5770f7994e528ebe6db8f
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 20:43:49 -0300

    update UserController

--- src/main/java/com/ecommerce/demo/controller/UserController.java
+++ src/main/java/com/ecommerce/demo/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ecommerce.demo.controller;
 
 // Import necessary classes for handling requests and responses in the controller
+import com.ecommerce.demo.dto.error.ErrorResponse;
 import com.ecommerce.demo.dto.request.UserRequest;
 import com.ecommerce.demo.dto.response.UserResponse;
 import com.ecommerce.demo.services.UserWriteServicesImpl;
 import com.ecommerce.demo.util.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,6 +26,7 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/users") // Define the base path for user-related requests
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     // Service to handle user creation logic
     private final UserWriteServicesImpl userWriteServices;
@@ -34,23 +39,30 @@ public class UserController {
 
     // Method to create a new user
     @PostMapping("/create") // Maps the POST request to create a user
-    public ResponseEntity<Result<?>> createUser(@RequestBody UserRequest request, BindingResult bindingResult) {
+    public ResponseEntity<?> createUser(@RequestBody UserRequest request, BindingResult bindingResult) {
+        Set<String> errorMessages = new HashSet<>();
         // Check for validation errors in the request
         if (bindingResult.hasErrors()) {
+            logger.error("Validation errors occurred while creating user: {}", bindingResult.getAllErrors());
             // Collect error messages and return them as a response
-            Set<String> errors = bindingResult.getAllErrors().stream()
+            errorMessages.addAll(bindingResult.getAllErrors().stream()
                     .map(ObjectError::getDefaultMessage)
-                    .collect(Collectors.toSet());
-            return ResponseEntity.badRequest().body(Result.failure(errors)); // Return a 400 Bad Request
+                    .collect(Collectors.toSet()));
+            return ResponseEntity.badRequest().body(
+                    new ErrorResponse("VALIDATION_ERROR", errorMessages, null)); // Return a 400 Bad Request
         }
 
         // Call the service to create the user and store the response
         Result<UserResponse> response = userWriteServices.create(request);
         // Check if user creation was successful
         if (response.isFailure()) {
-            return ResponseEntity.badRequest().body(Result.failure(response.getErrors())); // Return a 400 with errors
+            logger.error("User creation failed: {}", response.getErrors());
+            errorMessages.addAll(response.getErrors());
+            return ResponseEntity.badRequest().body(
+                    new ErrorResponse("User Creation Failed", errorMessages, null)); // Return a 400 with errors
         }
         // Return a 201 Created response if the user was successfully created
+        logger.info("User successfully created: {}", response);
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 Result.success("User successfully created: " + response)); // Return the success result
     }
