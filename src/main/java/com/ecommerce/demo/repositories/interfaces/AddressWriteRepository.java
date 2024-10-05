dd54ae41ef7eaec8f9f87df947adaebcb90e719f
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 23:02:32 -0300

    update AddressWriteRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/AddressWriteRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/AddressWriteRepository.java
@@ -1,10 +1,11 @@
-package com.ecommerce.demo.repositories;
+package com.ecommerce.demo.repositories.interfaces;
 
+import com.ecommerce.demo.dto.request.AddressRequest;
 import com.ecommerce.demo.entities.Address;
 import com.ecommerce.demo.util.Result;
 
 public interface AddressWriteRepository {
-    Result<Void> create(Address address);
+    Result<Void> create(Long userId, AddressRequest address);
     Result<Address> findById(Long id);
     Result<Void> delete(Long id);
 }
