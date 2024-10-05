7b5762f67877aaeb8668e8c2601afb20ad16bb57
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 23:03:31 -0300

    update AddressQueryRepositoryImpl

--- src/main/java/com/ecommerce/demo/repositories/AddressQueryRepositoryImpl.java
+++ src/main/java/com/ecommerce/demo/repositories/AddressQueryRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.ecommerce.demo.repositories;
 
 import com.ecommerce.demo.dto.request.AddressRequest;
 import com.ecommerce.demo.enums.DatabaseError;
+import com.ecommerce.demo.repositories.interfaces.AddressQueryRepository;
 import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +22,23 @@ public class AddressQueryRepositoryImpl implements AddressQueryRepository {
 
     @Override
     public Try<Result<Boolean>> exists(AddressRequest addressRequest) {
-        String sql = "SELECT EXISTS (SELECT 1 FROM Address " +
-                "WHERE street = ? AND street_number = ? AND city = ?" +
-                "AND state = ? AND postal_code = ? AND country = ?)";
+        String sql = "SELECT EXISTS (SELECT 1 FROM \"Address\" WHERE " +
+                "street = ? AND street_number = ? AND neighborhood = ? " +
+                "AND city = ? AND state = ? AND postal_code = ? AND country = ?)";
 
         return Try.of(() ->
-                        Optional.ofNullable(
-                                jdbcTemplate.queryForObject(sql, new Object[]{
-                                        addressRequest.getStreet(), addressRequest.getStreetNumber(),
-                                        addressRequest.getCity(), addressRequest.getState(),
-                                        addressRequest.getPostalCode(), addressRequest.getCountry()
-                                }, Boolean.class)
-                        )
-                        .orElse(false)
+                        jdbcTemplate.queryForObject(sql, new Object[]{
+                                addressRequest.getStreet(),
+                                addressRequest.getStreetNumber(),
+                                addressRequest.getCity(),
+                                addressRequest.getState(),
+                                addressRequest.getPostalCode(),
+                                addressRequest.getCountry()
+                        }, Boolean.class)
                 )
+                .map(result -> result != null && result)
                 .map(Result::success)
                 .recover(e -> Result.failure(
                         DatabaseError.QUERY_EXECUTION_ERROR.getMessage() + ": " + e.getMessage()));
-    }
+        }
 }
