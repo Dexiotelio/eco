2c8f9afe7203214d0deb057314ca4e83ad58bfe8
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 22:59:32 -0300

    update UserWriteRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/UserWriteRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/UserWriteRepository.java
@@ -1,10 +1,10 @@
-package com.ecommerce.demo.repositories;
+package com.ecommerce.demo.repositories.interfaces;
 
 import com.ecommerce.demo.entities.User;
 import com.ecommerce.demo.util.Result;
 
 public interface UserWriteRepository {
-    Result<Void> create(User user);
+    Result<Long> create(User user);
     void update(User user);
     void delete(Long id);
 }
