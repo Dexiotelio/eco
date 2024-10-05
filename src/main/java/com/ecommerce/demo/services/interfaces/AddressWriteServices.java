85cc95f89ca99f2a569c735e54636696d7e0251e
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 23:01:55 -0300

    update AddressWriteServices

--- src/main/java/com/ecommerce/demo/services/interfaces/AddressWriteServices.java
+++ src/main/java/com/ecommerce/demo/services/interfaces/AddressWriteServices.java
@@ -1,11 +1,11 @@
-package com.ecommerce.demo.services;
+package com.ecommerce.demo.services.interfaces;
 
 import com.ecommerce.demo.dto.request.AddressRequest;
 import com.ecommerce.demo.dto.response.AddressResponse;
 import com.ecommerce.demo.util.Result;
 
 public interface AddressWriteServices {
-    Result<AddressResponse> create(AddressRequest request);
+    Result<AddressResponse> create(Long userId, AddressRequest request);
     Result<AddressResponse> update(AddressRequest request);
     Result<AddressResponse>  delete(AddressRequest request);
 }
