2a698cd5b35ba2874440cd2fc09ba59b2b56731a
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-04 23:24:56 -0300

    update UserController

--- src/main/java/com/ecommerce/demo/controller/UserController.java
+++ src/main/java/com/ecommerce/demo/controller/UserController.java
@@ -13,15 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 // Controller responsible for user management
 @Controller
