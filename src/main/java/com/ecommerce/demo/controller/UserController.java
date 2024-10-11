3095c5ae1d817d32e1150b12a53c8ee68e9e1112
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-11 17:04:06 -0300

    update UserController

--- src/main/java/com/ecommerce/demo/controller/UserController.java
+++ src/main/java/com/ecommerce/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ecommerce.demo.controller;
 
 // Import necessary classes for handling requests and responses in the controller
 import com.ecommerce.demo.dto.error.ErrorResponse;
+import com.ecommerce.demo.dto.request.LoginRequest;
 import com.ecommerce.demo.dto.request.UserRequest;
 import com.ecommerce.demo.dto.response.UserResponse;
 import com.ecommerce.demo.services.UserWriteServicesImpl;
@@ -12,13 +13,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // Controller responsible for user management
-@Controller
+@RestController
 @RequestMapping("/users") // Define the base path for user-related requests
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -33,10 +34,11 @@ public class UserController {
     }
 
     // Method to create a new user
-    @PostMapping("/create") // Maps the POST request to create a user
+    @PostMapping("/register") // Maps the POST request to create a user
     public ResponseEntity<?> createUser(@Valid @RequestBody UserRequest request) {
         // Call the service to create the user and store the response
-        Result<UserResponse> response = userWriteServices.create(request);
+        Result<UserResponse> response = userWriteServices.registerUser(request);
+
         // Check if user creation was successful
         if (response.isFailure()) {
             logger.error("User creation failed: {}", response.getErrors());
@@ -44,9 +46,10 @@ public class UserController {
                     new ErrorResponse(
                             "User Creation Failed", response.getErrors(), null, null)); // Return a 400 with errors
         }
+
         // Return a 201 Created response if the user was successfully created
         logger.info("User successfully created: {}", response);
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                Result.success("User successfully created: " + response.toString())); // Return the success result
+                Result.success("User successfully created: " + response)); // Return the success result
     }
 }
