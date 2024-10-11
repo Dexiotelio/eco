2a31b3a86fccd53f1ac2d51b6e536d353bfc3e1f
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-11 00:22:17 -0300

    update UserQueryRepository

--- src/main/java/com/ecommerce/demo/repositories/interfaces/UserQueryRepository.java
+++ src/main/java/com/ecommerce/demo/repositories/interfaces/UserQueryRepository.java
@@ -1,6 +1,6 @@
 package com.ecommerce.demo.repositories.interfaces;
 
-import com.ecommerce.demo.entities.User;
+import com.ecommerce.demo.model.User;
 import com.ecommerce.demo.util.Result;
 import io.vavr.control.Try;
 
@@ -8,4 +8,5 @@ public interface UserQueryRepository {
     User findById(Long id);
     Try<Result<Boolean>> exists(String email);
     Try<Result<Boolean>> existsUsername(String username);
+    Try<Result<User>> findByUsername(String username);
 }
