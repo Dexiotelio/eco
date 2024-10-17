da994e7457dadea88b63487903096a38afb0ef43
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-17 02:51:20 -0300

    update AddressWriteServicesImpl

--- src/main/java/com/ecommerce/demo/services/AddressWriteServicesImpl.java
+++ src/main/java/com/ecommerce/demo/services/AddressWriteServicesImpl.java
@@ -7,6 +7,7 @@ import com.ecommerce.demo.repositories.AddressQueryRepositoryImpl;
 import com.ecommerce.demo.repositories.AddressWriteRepositoryImpl;
 import com.ecommerce.demo.services.interfaces.AddressWriteServices;
 import com.ecommerce.demo.util.Result;
+import com.ecommerce.demo.validation.AddressValidation;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import org.slf4j.Logger;
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Set;
 
@@ -25,47 +27,52 @@ public class AddressWriteServicesImpl implements AddressWriteServices {
     private final AddressWriteRepositoryImpl addressWriteRepository;
     // Repository for querying addresses
     private final AddressQueryRepositoryImpl addressQueryRepository;
+    private final TransactionTemplate transactionTemplate;
 
     // Constructor that injects the repositories
     @Autowired
     public AddressWriteServicesImpl(AddressWriteRepositoryImpl addressWriteRepository,
-                                    AddressQueryRepositoryImpl addressQueryRepository) {
+                                    AddressQueryRepositoryImpl addressQueryRepository,
+                                    TransactionTemplate transactionTemplate) {
         this.addressWriteRepository = addressWriteRepository;
         this.addressQueryRepository = addressQueryRepository;
+        this.transactionTemplate = transactionTemplate;
     }
 
     // Method to create a new address for a user
     @Override
-    @Transactional // Indicates that the method should be executed within a transaction
     public Result<AddressResponse> create(Long userId, AddressRequest request) {
-        // Check if the address already exists
-        Either<Set<String>, AddressRequest> addressValidation = AddressValidation.validateAddressRequest(request);
-        if (addressValidation.isLeft()) {
-            return Result.failure(addressValidation.getLeft());
-        }
+        return transactionTemplate.execute(status -> {
+            // Check if the address already exists
+            Either<Set<String>, AddressRequest> addressValidation = AddressValidation.validateAddressRequest(request);
+            if (addressValidation.isLeft()) {
+                return Result.failure(addressValidation.getLeft());
+            }
 
-        Try<Result<Boolean>> addressExists = addressQueryRepository.exists(request);
-        if (addressExists.isFailure()) {
-            return Result.failure(addressExists.getCause().getMessage()); // Return failure if query fails
-        }
+            Try<Result<Boolean>> addressExists = addressQueryRepository.exists(request);
+            if (addressExists.isFailure()) {
+                return Result.failure(addressExists.getCause().getMessage()); // Return failure if query fails
+            }
 
-        Result<Boolean> addressExistsResult = addressExists.get();
-        // If the address already exists, return an error
-        if (addressExistsResult.isSuccess() && Boolean.TRUE.equals(addressExistsResult.getValue())) {
-            return Result.failure(AddressErrorCode.ADDRESS_ALREADY_EXISTS.getMessage());
-        }
+            Result<Boolean> addressExistsResult = addressExists.get();
+            // If the address already exists, return an error
+            if (addressExistsResult.isSuccess() && Boolean.TRUE.equals(addressExistsResult.getValue())) {
+                return Result.failure(AddressErrorCode.ADDRESS_ALREADY_EXISTS.getMessage());
+            }
 
-        // Attempt to create the address in the repository
-        Result<Void> addressCreationResult = addressWriteRepository.create(userId, request);
-        if (addressCreationResult.isFailure()) {
-            // Return failure if address creation fails, including any errors
-            return Result.failure(AddressErrorCode.ADDRESS_CREATION_FAILURE.getMessage() +
-                    ": " + String.join(", ", addressCreationResult.getErrors()));
-        }
+            // Attempt to create the address in the repository
+            Result<Void> addressCreationResult = addressWriteRepository.create(userId, request);
+            if (addressCreationResult.isFailure()) {
+                // Return failure if address creation fails, including any errors
+                status.setRollbackOnly();
+                return Result.failure(AddressErrorCode.ADDRESS_CREATION_FAILURE.getMessage() +
+                        ": " + String.join(", ", addressCreationResult.getErrors()));
+            }
 
-        // Convert the Address entity to an AddressResponse and return success
-        AddressResponse response = AddressResponse.toAddressResponse(request);
-        return Result.success(response);
+            // Convert the Address entity to an AddressResponse and return success
+            AddressResponse response = AddressResponse.toAddressResponse(request);
+            return Result.success(response);
+        });
     }
 
     // Method to update an existing address (not implemented)
